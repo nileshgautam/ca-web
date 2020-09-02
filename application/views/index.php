3f4d6d4e2c2d59e7a68a036318647f0b21e8b641
Nilesh_office <nileshwephyre@gmail.com>
2020-09-02 10:25:50 +0530

    Home page design ready

--- application/views/index.php
+++ application/views/index.php
@@ -1,22 +1,92 @@
-<section id="header" class="container-fluid">
-	<div class="row">
-		<div class="col-sm-12">
-			<nav class="navbar">
-				<div class="logo">
-					<img src="<?php echo base_url('assets/') ?>image/logo.png" alt="ca.com">
-				</div>
-				<ul>
-					<li><a href="">Business Startups</a> </li>
-					<li><a href="">GST Registration</a> </li>
-					<li><a href="">Income Tax</a></li>
-					<li><a href="">Compliance</a></li>
+<!-- Header start -->
 
-				</ul>
-			</nav>
+<header id="header" class="container-fluid">
+	<div class="col-sm-12">
+		<nav class="navbar">
+
+			<div class="logo">
+				<img src="<?php echo base_url('assets/') ?>image/logo.png" alt="ca.com">
+			</div>
+			<ul>
+				<li><a href="">Business Startups</a> </li>
+				<li><a href="">GST Registration</a> </li>
+				<li><a href="">Income Tax</a></li>
+				<li><a href="">Compliance</a></li>
+			</ul>
+
+		</nav>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-6 ">
+			<div class="head-para">
+				<h4>The Trusted leader for Businesses</h4>
+				<h1>#1 Online Company Setup
+					Services in India</h1>
+				<div class="get-free">
+					<h5>Get Free Consultation Now</h5>
+					<form action="" class="action-form">
+						<div class="action-button-one col-sm-8"> <input type="text" placeholder="Your Email Address" class="input-style-2"> <a href="#" class="btn">Consult Online <img src="<?php echo base_url('assets/image/icon/next.png') ?>" alt="" height="12"> </a></div>
+					</form>
+				</div>
+			</div>
+
+
+		</div>
+		<div class="col-sm-6">
+			<div><img src="<?php echo base_url('assets/image/index/bg2.png') ?>" alt="" height="" width="100%"></div>
+		</div>
+	</div>
+</header>
+
+<!-- Register, Form, Build
+Or Grow your Company in -->
+
+<section class="">
+	<div class="container city">
+		<div class="row ">
+			<div class="col-sm-3 pl-5">
+				<h5 class="title-2">Register, Form, Build Or Grow your Company in</h5>
+				<p class="text-danger title-3 ">Trusted by over 10,000 business owners worldwide</p>
+			
+			</div>
+			<div class="col-sm-9 bg-white">
+				<div class="recent-city">
+					<div class="card">
+						<img src="<?php echo base_url('assets/image/cityimages/india.jpg') ?>" alt="">
+						<div class="title">India</div>
+					</div>
+					<div class="card">
+						<img src="<?php echo base_url('assets/image/cityimages/india.jpg') ?>" alt="">
+						<div class="title">India</div>
+					</div>
+					<div class="card">
+						<img src="<?php echo base_url('assets/image/cityimages/india.jpg') ?>" alt="">
+						<div class="title">India</div>
+					</div>
+					<div class="card">
+						<img src="<?php echo base_url('assets/image/cityimages/india.jpg') ?>" alt="">
+						<div class="title">India</div>
+					</div>
+					<div class="card">
+						<img src="<?php echo base_url('assets/image/cityimages/india.jpg') ?>" alt="">
+						<div class="title">India</div>
+					</div>
+					<div class="card">
+						<img src="<?php echo base_url('assets/image/cityimages/india.jpg') ?>" alt="">
+						<div class="title">India</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
 
+
+
+<!-- Header End -->
+
+<!-- Our services start -->
 <section id="our-services" class="container">
 	<div class="row">
 		<div class="col-sm-12">
@@ -27,8 +97,7 @@
 
 			<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
 				<li class="nav-item">
-					<a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-business-registrations
-" role="tab" aria-controls="pills-home" aria-selected="true">Business Registrations
+					<a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-business-registrations" role="tab" aria-controls="pills-home" aria-selected="true">Business Registrations
 					</a>
 				</li>
 
@@ -61,12 +130,12 @@
 									<h6 class="card-title">Proprietorship Company</h6>
 									<label for="packages">Packages</label>
 									<form action="" class="p-form">
-										<input type="radio"  name="packages" id="" checked>₹ 2,899 
-										<input type="radio" name="packages" id="">₹ 6,899  
+										<input type="radio" name="packages" id="" checked>₹ 2,899
+										<input type="radio" name="packages" id="">₹ 6,899
 										<input type="radio" name="packages" id="">₹ 12,899
 										<div>
-										<small>Prices Inclusive of all taxes</small>
-										<button class="c-btn">Get Started</button>
+											<small>Prices Inclusive of all taxes</small>
+											<button class="c-btn">Get Started</button>
 										</div>
 									</form>
 								</div>
@@ -80,12 +149,12 @@
 									<h6 class="card-title">Proprietorship Company</h6>
 									<label for="packages">Packages</label>
 									<form action="" class="p-form">
-										<input type="radio"  name="packages" id="" checked>₹ 2,899 
-										<input type="radio" name="packages" id="">₹ 6,899  
+										<input type="radio" name="packages" id="" checked>₹ 2,899
+										<input type="radio" name="packages" id="">₹ 6,899
 										<input type="radio" name="packages" id="">₹ 12,899
 										<div>
-										<small>Prices Inclusive of all taxes</small>
-										<button class="c-btn">Get Started</button>
+											<small>Prices Inclusive of all taxes</small>
+											<button class="c-btn">Get Started</button>
 										</div>
 									</form>
 								</div>
@@ -98,12 +167,12 @@
 									<h6 class="card-title">Proprietorship Company</h6>
 									<label for="packages">Packages</label>
 									<form action="" class="p-form">
-										<input type="radio"  name="packages" id="" checked>₹ 2,899 
-										<input type="radio" name="packages" id="">₹ 6,899  
+										<input type="radio" name="packages" id="" checked>₹ 2,899
+										<input type="radio" name="packages" id="">₹ 6,899
 										<input type="radio" name="packages" id="">₹ 12,899
 										<div>
-										<small>Prices Inclusive of all taxes</small>
-										<button class="c-btn">Get Started</button>
+											<small>Prices Inclusive of all taxes</small>
+											<button class="c-btn">Get Started</button>
 										</div>
 									</form>
 								</div>
@@ -134,7 +203,18 @@
 		</div>
 	</div>
 </section>
+<!-- Our services end -->
+<!-- Counter start -->
+<section id="counter" class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="section-heading">Counter</h2>
+		</div>
+	</div>
+</section>
+<!-- Counter end -->
 
+<!-- Why choose us start -->
 <section id="why-choose-us" class="container">
 	<div class="row">
 		<div class="col-sm-12 py-5">
@@ -211,7 +291,9 @@
 	</div>
 
 </section>
+<!-- Why choose us end -->
 
+<!-- Our client start -->
 <section class="clients-container container">
 	<div class="row">
 		<div class="col-sm-12">
@@ -231,7 +313,9 @@
 		<div class="col-sm-3"><img src="<?php echo base_url('assets/image/c8.png') ?>" alt=""></div>
 	</div>
 </section>
+<!-- Our client end -->
 
+<!-- Our testimonial start -->
 <section id="client-review" class="container">
 	<div class="row">
 		<div class="col-sm-12">
@@ -243,7 +327,7 @@
 			<div class="card">
 				<img class="card-img-top" src="<?php echo base_url('assets/image/user.png') ?>" alt="Card image cap">
 				<div class="card-body">
-					<h6 class="card-title text-primary">Rahul Shrivastava</h6>
+					<h6 class="card-title">Rahul Shrivastava</h6>
 					<p class="">CEO, Ecommerce Mart</p>
 
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -261,7 +345,7 @@
 			<div class="card">
 				<img class="card-img-top" src="<?php echo base_url('assets/image/user.png') ?>" alt="Card image cap">
 				<div class="card-body">
-					<h6 class="card-title text-primary">Piyush Mehra</h6>
+					<h6 class="card-title">Piyush Mehra</h6>
 					<p class="">CEO, Ecommerce Mart</p>
 
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -279,7 +363,7 @@
 			<div class="card">
 				<img class="card-img-top" src="<?php echo base_url('assets/image/user.png') ?>" alt="Card image cap">
 				<div class="card-body">
-					<h6 class="card-title text-primary">Ritika Sharma</h6>
+					<h6 class="card-title">Ritika Sharma</h6>
 					<p class="">CEO, Ecommerce Mart</p>
 
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -297,7 +381,7 @@
 			<div class="card">
 				<img class="card-img-top" src="<?php echo base_url('assets/image/user.png') ?>" alt="Card image cap">
 				<div class="card-body">
-					<h6 class="card-title text-primary">Neha Bhatnagar</h6>
+					<h6 class="card-title">Neha Bhatnagar</h6>
 					<p class="">CEO, Ecommerce Mart</p>
 
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -314,3 +398,185 @@
 
 	</div>
 </section>
+<!-- Our testimonial end -->
+
+<!-- start Ready to get started? -->
+<section id="contact-us">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="call-to-action">
+				<h2 class="">Ready to get started?</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, dolore.</p>
+				<a class="a-btn-primary">Book A Free Video Consulation</a>
+				<a class="a-btn-secondary">Contact Us</a>
+			</div>
+
+		</div>
+	</div>
+</section>
+<!-- end Ready to get started? -->
+
+<!-- start footer -->
+<footer class="Container">
+	<!-- Footer logo section -->
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 row">
+				<div class="col-sm-2"><img src="<?php echo base_url('assets/') ?>image/logo.png" alt="logo" class="footer-logo"></div>
+				<div class="col-sm-10"> <strong>CA Website</strong> </div>
+			</div>
+			<div class="col-sm-8 row">
+				<div class="col-sm-5 offset-9">
+					<span> <img src="<?php echo base_url('assets/image/icon/country.png') ?>" alt="india" class="i-18"> India</span>
+					<span> <img src="<?php echo base_url('assets/image/icon/language.png') ?>" alt="language" class="i-16"> English (India)</span>
+
+				</div>
+
+			</div>
+		</div>
+		<hr class="divider">
+	</div>
+	<!-- footer service link start -->
+	<div class="footer-service-link container">
+		<div class="row">
+			<div class="col-sm-3">
+				<span class="service-title-danger">Start your</span>
+				<ul>
+					<li><a href="">Proprietorship Company</a></li>
+					<li><a href="">Partnership Company</a></li>
+					<li><a href="">Limited Liability Partnership</a></li>
+					<li><a href="">Private Limited Company</a></li>
+					<li><a href="">Public Limited Company</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-3">
+				<span class="service-title-danger">GST Services</span>
+				<ul>
+					<li><a href="">Proprietorship Company</a></li>
+					<li><a href="">Partnership Company</a></li>
+					<li><a href="">Limited Liability Partnership</a></li>
+					<li><a href="">Private Limited Company</a></li>
+					<li><a href="">Public Limited Company</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-3"><span class="service-title-danger">Income Tax</span>
+				<ul>
+					<li><a href="">Proprietorship Company</a></li>
+					<li><a href="">Partnership Company</a></li>
+					<li><a href="">Limited Liability Partnership</a></li>
+					<li><a href="">Private Limited Company</a></li>
+					<li><a href="">Public Limited Company</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-3"><span class="service-title-danger">Corporate Filings</span>
+				<ul>
+					<li><a href="">Proprietorship Company</a></li>
+					<li><a href="">Partnership Company</a></li>
+					<li><a href="">Limited Liability Partnership</a></li>
+					<li><a href="">Private Limited Company</a></li>
+					<li><a href="">Public Limited Company</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- Footer Address starts -->
+	<div class="container-fluid footer-address-section">
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-12">
+						<span class="address-title-primary">Bangalore Office</span>
+					</div>
+				</div>
+
+				<div class="address">
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/location.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">The Business Centre 61 Wellfield Road, Roath Cardiff</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/phone.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">
+							<p class="address-phone">080-8758652 | +91 98765 43210</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/mail.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">contactus@email.com</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<span class="address-title-primary">Delhi Office</span>
+				<div class="address">
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/location.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">The Business Centre 61 Wellfield Road, Roath Cardiff</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/phone.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">
+							<p class="address-phone">080-8758652 | +91 98765 43210</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/mail.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">contactus@email.com</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<span class="address-title-primary">Singapore Office</span>
+				<div class="address">
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/location.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">The Business Centre 61 Wellfield Road, Roath Cardiff</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/phone.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">
+							<p class="address-phone">080-8758652 | +91 98765 43210</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/mail.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">contactus@email.com</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<span class="address-title-primary">UAE Office</span>
+				<div class="address">
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/location.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">The Business Centre 61 Wellfield Road, Roath Cardiff</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/phone.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">
+							<p class="address-phone">080-8758652 | +91 98765 43210</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-2"> <span><img src="<?php echo base_url('assets/image/icon/mail.png') ?>" alt="location" class="h-15"></span></div>
+						<div class="col-sm-10">contactus@email.com</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<nav class="footer-nav">
+			<ul>
+				<li> <a href="#">Home</a> </li>
+				<li> <a href="#">About Us</a> </li>
+				<li> <a href="#">Career</a> </li>
+				<li> <a href="#">Accounting Calendar</a> </li>
+				<li> <a href="#">Contact Us</a> </li>
+				<li> <a href="#">Privacy Policy</a> </li>
+				<li> <a href="#">Terms & Conditions</a> </li>
+				<li> <a href="#">Refund Policy</a> </li>
+			</ul>
+		</nav>
+	</div>
+</footer>
+<!-- end footer -->
