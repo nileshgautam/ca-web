c00052819e299e156f9e9d4dc51ce44e3a7e2e14
YatharthSharma-21 <as0768299@gmail.com>
2020-09-07 15:40:32 +0530

    user controller and multilogin

--- application/views/login/header.php
+++ application/views/login/header.php
@@ -10,22 +10,20 @@
   <meta name="author" content="">
 
   <title>Login</title>
-  <!-- Custom fonts for this template-->
-  <link href="<?php echo base_url('assets/admin/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+  <!-- Custom fonts for this template-->  
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="<?php echo base_url('assets/admin/css/sb-admin-2.min.css') ?>" rel="stylesheet">
- <!-- Bootstrap core JavaScript-->
- <script src="<?php echo base_url('assets/admin/vendor/jquery/jquery.min.js') ?>"></script>
-  <!-- Bootstrap Notify -->
-<script type="text/javascript" src="<?php echo base_url('assets/admin/plugins/bootstrap-notify/bootstrap-notify.min.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/admin/js/helper.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/admin/js/constants.js'); ?>"></script>
-<script>
-const BASE_URL="<?php echo base_url()?>";
-
-</script>
+  <link href="<?php echo base_url('assets/css/index.css') ?>" rel="stylesheet">
+  <!-- jQuery -->
+  <script src="<?php echo base_url() ?>adminAssets/js/jquery.min.js"></script>
+  <!-- Notiflix for alert messages -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>adminAssets/notiflix/notiflix-2.4.0.min.css" />
+  <script src="<?php echo base_url() ?>adminAssets/notiflix/notiflix-2.4.0.min.js"></script>
+  <script>
+    const BASE_URL = "<?php echo base_url() ?>";
+  </script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 
 <body class="bg-gradient-primary">
